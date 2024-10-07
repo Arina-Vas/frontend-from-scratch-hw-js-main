c656a575b11b4f3549766d76bf5951c8e97a5b06
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-07 14:26:06 +0200

    lesson-04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,13 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array.includes(element)) {
+            return true;
+        } else {
+            return false
+        }
+    }
+}
+console.log(includesElement([1, 2, 3], 2));
