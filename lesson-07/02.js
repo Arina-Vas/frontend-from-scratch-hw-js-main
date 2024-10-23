6d7035be4487bf25f8122e0bb24bc3b7c085d0d4
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-23 14:26:28 +0200

    lesson-07/02-solution

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,11 +4,23 @@
  */
 
 function isNumeric(str) {
-  // your code
+  if (str === " " || str === "") {
+    return false;
+  }
+  else {
+    let result = Number(str);
+    if (Number.isNaN(result)) {
+      return false;
+    }
+  }
+  return true;
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+
+console.log(Number("abc"));
