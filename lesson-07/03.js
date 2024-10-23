3f750c5008750f967e78624c76493f508eeca855
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-23 14:32:17 +0200

    lesson-07/03-solution

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -10,5 +10,11 @@
 */
 
 function truncate(str, maxLength) {
-  // your code
+  if (str.length <= maxLength) {
+    return str;
+  }
+  let resultStr = str.slice(0, maxLength) + "...";
+  return resultStr;
 }
+console.log(truncate("Вот, что мне действительно нравится в этом", 20));
+console.log(truncate("Короткая строка", 20));
