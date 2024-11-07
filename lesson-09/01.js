c715b5054d969072b5dab42b9fd96693acccf36a
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-11-07 12:53:55 +0100

    lesson-09/01-test2

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -25,7 +25,7 @@ console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 const filter = (array, callback) => {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) {
+    if (callback(array[i], i)) {
       newArray.push(array[i])
     }
   }
